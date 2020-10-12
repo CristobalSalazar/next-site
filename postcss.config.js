dfa8c7c5821773bab22f5691efdd6dac3f541169
Cristobal Salazar <chris.salza@gmail.com>
2020-10-12 12:45:23 -0400

    updated postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,19 @@
+function getPurgeCssPlugin() {
+  if (process.env.NODE_ENV === "production") {
+    return [
+      "@fullhuman/postcss-purgecss",
+      {
+        content: ["./src/pages/*.tsx", "./src/components/*.tsx"],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      },
+    ];
+  }
+}
 module.exports = {
-  plugins: ["tailwindcss", "postcss-preset-env"],
+  plugins: [
+    "tailwindcss",
+    "postcss-preset-env",
+    getPurgeCssPlugin(),
+    "autoprefixer",
+  ],
 };
