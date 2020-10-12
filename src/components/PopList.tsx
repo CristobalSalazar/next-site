e3d4ea0e7354cf09cea472bd2cae8f9c302462dd
Cristobal Salazar <chris.salza@gmail.com>
2020-10-12 12:30:23 -0400

    changed colors

--- src/components/PopList.tsx
+++ src/components/PopList.tsx
@@ -4,10 +4,14 @@ interface PopListProps {
 }
 export default function PopList({ items, showAnimation = true }: PopListProps) {
   return (
-    <ul className="w-full mx-auto rounded sm:m-0 text-dark">
+    <ul className="w-full mx-auto rounded-lg overflow-hidden sm:m-0 shadow-lg border border-primary">
       {items.map((item, index) => (
         <li
-          className="z-0 p-6 text-left bg-white transition-colors duration-300 hover:bg-primary shadow-md border-b"
+          className={`p-6 text-left ${
+            index % 2 === 0
+              ? "bg-light text-dark hover:bg-secondary"
+              : "bg-dark text-light hover:bg-gray-700"
+          } md:rounded transition-colors duration-300`}
           key={index}
         >
           {item}
