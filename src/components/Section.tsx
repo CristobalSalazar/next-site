0cfa8ed6541d324f13ebd7a5f9f5f50272b99ae3
Cristobal Salazar <chris.salza@gmail.com>
2020-10-12 12:30:44 -0400

    rm id

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -5,7 +5,7 @@ interface SectionProps {
 }
 const Section: FC<SectionProps> = ({ children, title }) => {
   return (
-    <section className="py-12 sm:px-3" id={title.toLowerCase()}>
+    <section className="py-12 sm:px-3">
       <h3 className="mb-6 text-2xl capitalize font-cursive text-accent sm:text-center sm:text-3xl">
         {title}
       </h3>
