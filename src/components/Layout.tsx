e043dc960672c7c3bcf9802d3cc32b7cc45e6143
Cristobal Salazar <chris.salza@gmail.com>
2020-10-12 12:30:16 -0400

    added fade in for footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -33,44 +33,46 @@ export default function Layout(props: LayoutProps) {
         ]}
       />
       <div className="bg-light">{props.children}</div>
-      <Footer
-        sections={[
-          {
-            name: "About",
-            links: [
-              { name: "Welcome", href: "/#welcome" },
-              { name: "Skills", href: "/#skills" },
-              { name: "Education", href: "/#education" },
-            ],
-          },
-          {
-            name: "Portfolio",
-            links: [
-              { name: "Web", href: "portfolio#web" },
-              { name: "Unity", href: "portfolio/#unity" },
-              { name: "Console", href: "portfolio/#console" },
-            ],
-          },
-          {
-            name: "Social",
-            links: [
-              {
-                name: "Github",
-                href: "https://www.github.com/cristobalsalazar",
-              },
-              {
-                name: "LinkedIn",
-                href:
-                  "https://www.linkedin.com/in/cristobal-salazar-849960164/",
-              },
-            ],
-          },
-          {
-            name: "Contact",
-            links: [{ name: "Email", href: "mailto:chris.salza@gmail.com" }],
-          },
-        ]}
-      />
+      <ScrollFadeIn>
+        <Footer
+          sections={[
+            {
+              name: "About",
+              links: [
+                { name: "Welcome", href: "/#welcome" },
+                { name: "Skills", href: "/#skills" },
+                { name: "Education", href: "/#education" },
+              ],
+            },
+            {
+              name: "Portfolio",
+              links: [
+                { name: "Web", href: "portfolio#web" },
+                { name: "Unity", href: "portfolio/#unity" },
+                { name: "Console", href: "portfolio/#console" },
+              ],
+            },
+            {
+              name: "Social",
+              links: [
+                {
+                  name: "Github",
+                  href: "https://www.github.com/cristobalsalazar",
+                },
+                {
+                  name: "LinkedIn",
+                  href:
+                    "https://www.linkedin.com/in/cristobal-salazar-849960164/",
+                },
+              ],
+            },
+            {
+              name: "Contact",
+              links: [{ name: "Email", href: "mailto:chris.salza@gmail.com" }],
+            },
+          ]}
+        />
+      </ScrollFadeIn>
     </>
   );
 }
