cd59242ca6456152624dc6c0b9147d6a16dbb15c
Cristobal Salazar <chris.salza@gmail.com>
2020-10-12 13:55:37 -0400

    better mobile nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,27 +7,25 @@ interface NavProps {
 export default function Nav({ title, links }: NavProps) {
   return (
     <>
-      <nav className="fixed z-10 flex justify-between w-screen p-3 bg-transparent bg-light shadow-md">
-        <div className="flex ml-4">
-          <span className="box-border pr-4 text-dark font-cursive h-100 sm:hidden">
-            {title}
-          </span>
-          <ul className="ml-4 sm:ml-0">
-            {links.map((link) => (
-              <li className="inline-block mr-6" key={link.name}>
-                <Link href={link.route}>
-                  <a
-                    className={`${
-                      link.active ? "text-accent" : "text-dark"
-                    } hover:text-accent transition-colors duration-300`}
-                  >
-                    {link.name}
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <nav className="fixed z-10 flex w-screen p-3 bg-transparent shadow-md bg-light">
+        <span className="box-border flex-shrink-0 pr-4 text-dark font-cursive h-100 sm:hidden">
+          {title}
+        </span>
+        <ul className="flex w-full ml-4 sm:ml-0 sm:justify-around">
+          {links.map((link) => (
+            <li className="mr-6 sm:mr-0" key={link.name}>
+              <Link href={link.route}>
+                <a
+                  className={`${
+                    link.active ? "text-accent" : "text-dark"
+                  } hover:text-accent transition-colors duration-300`}
+                >
+                  {link.name}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </nav>
     </>
   );
