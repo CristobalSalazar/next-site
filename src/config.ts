e51939e20544347e6bcfa32481794380a9697ad7
Cristobal Salazar <chris.salza@gmail.com>
2020-10-14 16:21:08 -0400

    ignore env local

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,6 @@
-export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:1337";
+export const BACKEND_URL =
+  process.env.BACKEND_URL ||
+  process.env.DEV_BACKEND_URL ||
+  "http://localhost:1337";
+
+console.log("Backend url", BACKEND_URL);
