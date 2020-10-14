69a4f7114ba0a71006a81157fdc27d61ac92f119
Cristobal Salazar <chris.salza@gmail.com>
2020-10-14 12:41:31 -0400

    corrected font link

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,12 +5,12 @@ const Layout: React.FC<LayoutProps> = ({ children }) => (
   <>
     <Head>
       <link
-        href="https://fonts.googleapis.com/css2?family=Inter&David+Libre&family=Playfair+Display&family=Sansita+Swashed&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Inter&family=David+Libre&family=Playfair+Display&family=Sansita+Swashed&display=swap"
         rel="stylesheet"
       />
       <link rel="icon" type="image/png" href="/favicon.png" />
     </Head>
-    <div className="bg-light">{children}</div>
+    {children}
   </>
 );
 export default Layout;
