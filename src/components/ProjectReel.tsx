a68e0f945cec0ec21df7280a8442a2a5f88491be
Cristobal Salazar <cristobals@i3inc.ca>
2020-10-17 12:03:52 -0400

    added bg-vid

--- src/components/ProjectReel.tsx
+++ src/components/ProjectReel.tsx
@@ -4,46 +4,44 @@ import { getRandomArrEl } from "../utils/array";
 export interface ProjectReelProps {
   projects: Project[];
 }
-const ProjectReel: React.FC<ProjectReelProps> = ({ projects }) => {
-  const randomProject = getRandomArrEl(projects);
-  return (
-    <>
-      <video
-        style={{ filter: "grayscale(100%)" }}
-        src={randomProject.files[0].url}
-        autoPlay={true}
-        muted={true}
-        loop
-        className="absolute z-10 object-cover w-screen h-screen opacity-25"
-      />
-      <div className="z-20 flex flex-col h-screen py-12">
-        <h1
-          style={{ letterSpacing: "4ch" }}
-          className="z-20 mb-6 text-4xl text-center uppercase text-light"
-        >
-          Projects
-        </h1>
-        <ul className="grid h-full grid-cols-6 mx-32 " id="projects">
-          {projects.map((p) => (
-            <li className="z-20 cursor-pointer bg-dark" key={p.title}>
-              <video
-                autoPlay={true}
-                loop
-                onMouseEnter={(e) => {
-                  e.currentTarget.style.filter = "none";
-                }}
-                onMouseLeave={(e) => {
-                  e.currentTarget.style.filter = "grayscale(100%)";
-                }}
-                className="object-cover w-full h-full duration-200 rounded-lg"
-                muted
-                src={p.files[0].url}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
-};
+const ProjectReel: React.FC<ProjectReelProps> = ({ projects }) => (
+  <>
+    <video
+      style={{ filter: "grayscale(100%)" }}
+      src={getRandomArrEl(projects).files[0].url}
+      autoPlay={true}
+      muted={true}
+      loop
+      className="absolute z-0 object-cover w-screen h-screen min-h-screen overflow-hidden opacity-25 sm:hidden"
+    />
+    <div className="z-20 flex flex-col h-screen min-h-screen py-12 sm:py-0">
+      <h1
+        style={{ letterSpacing: "4ch" }}
+        className="z-20 mb-6 text-4xl text-center uppercase sm:text-xl text-light"
+      >
+        Projects
+      </h1>
+      <ul
+        className="grid h-full grid-cols-6 mx-32 sm:block sm:mx-0"
+        id="projects"
+      >
+        {projects.map((p) => (
+          <li
+            className="z-20 rounded-lg cursor-pointer bg-primary"
+            key={p.title}
+          >
+            <video
+              style={{ filter: "grayscale(100%)" }}
+              autoPlay={true}
+              loop
+              className="object-cover w-full h-full transition-opacity duration-300 hover:opacity-50"
+              muted
+              src={p.files[0].url}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  </>
+);
 export default ProjectReel;
