2f932f81a5104f1992b40307ac1744808d07f0a1
Cristobal Salazar <cristobals@i3inc.ca>
2020-10-17 12:04:19 -0400

    add margin for sm screens

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 import Text from "./Text";
 
 const Grid: React.FC = ({ children }) => (
-  <div className="grid grid-cols-3 gap-12 md:mb-0 md:block sm:block">
+  <div className="grid grid-cols-3 gap-12 md:mb-0 md:block sm:block sm:mx-auto sm:px-3">
     {children}
   </div>
 );
@@ -19,12 +19,14 @@ const Welcome: React.FC<WelcomeProps> = ({
 }) => (
   <Grid>
     <img
-      className="object-cover w-64 h-64 mx-auto border rounded-full shadow-xl border-dark"
+      className="object-cover w-64 h-64 mx-auto border rounded-full shadow-xl border-dark sm:mb-6"
       src={profilePicUrl}
       alt="profile_pic"
     />
     <div className="col-span-2">
-      <h2 className="mb-3 text-4xl text-dark">{slogan}</h2>
+      <h2 className="mb-3 text-4xl sm:mb-6 text-dark sm:text-center sm:text-3xl">
+        {slogan}
+      </h2>
       <Text>{paragraph}</Text>
       <div className="flex justify-end mt-6">
         <Button>Contact Me</Button>
