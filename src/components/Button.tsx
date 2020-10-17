6a343203cad5b2693b303b8ca9f58ea2a5c4274a
Cristobal Salazar <cristobals@i3inc.ca>
2020-10-17 12:03:20 -0400

    rm outline

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ type Props = React.DetailedHTMLProps<
 const Button: React.FC<Props> = ({ children, className, ...rest }) => (
   <button
     {...rest}
-    className={`rounded-l-full rounded-r-full px-3 py-2 transition-colors duration-200 hover:shadow-md border outline-none border-accent text-accent hover:bg-accent hover:text-light ${className}`}
+    className={`focus:outline-none rounded-l-full rounded-r-full px-3 py-2 transition-colors duration-200 hover:shadow-md border outline-none border-accent text-accent hover:bg-accent hover:text-light ${className}`}
   >
     {children}
   </button>
